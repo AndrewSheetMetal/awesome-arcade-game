d1de7eafeba0289260e47ad02cd8e19d67a8c0ea
Sven <Sven@141.71.94.23>
2015-10-09 10:06:53 +0200

    //TODO entfernt

--- awesome-arcade-game/de/hsh/Main.java
+++ awesome-arcade-game/de/hsh/Main.java
@@ -6,7 +6,6 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		System.out.println("Hier ist Sven");
 		System.out.println("Hier ist Änderung");
