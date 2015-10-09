26ab5722f8425ee388a1c2ee350039c6f6740789
philippgottschalk <philippgottschalk@141.71.91.110>
2015-10-09 10:27:17 +0200

    Kleine Änderung

--- awesome-arcade-game/de/hsh/Main.java
+++ awesome-arcade-game/de/hsh/Main.java
@@ -11,5 +11,6 @@ public class Main {
 		System.out.println("Hier ist Andi");
 		System.out.println("Hier ist Änderung");
 		System.out.println("Hier ist Basti");
+		System.out.println("Hier ist Philipp");
 	}
 }
