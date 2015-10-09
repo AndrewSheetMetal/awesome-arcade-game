37dec5a93a1c449fa9485d54ea44376befc09389
Sven <Sven@LenovoY50-70>
2015-10-09 17:30:01 +0200

    Cockens scheiss main methode entfernt

--- awesome-arcade-game/de/hsh/Player.java
+++ awesome-arcade-game/de/hsh/Player.java
@@ -1,13 +1,7 @@
 package de.hsh;
 
 public class Player {
-
-	//Es gibt nur EINE main Methode
-	
-	public static void main(String[] args) {
-		System.out.println("sieg");
+	public Player(){
+		
 	}
-	
-	
-	
 }
