6b2194d6b4840cb4de3f7299ce5ea2616fe3c9e1
andreas <andreas@andreas-Aspire-5749>
2015-10-26 09:50:22 +0100

    paintComponent überschrieben

--- awesome-arcade-game/de/hsh/GameScreen.java
+++ awesome-arcade-game/de/hsh/GameScreen.java
@@ -1,20 +1,31 @@
 package de.hsh;
 
+import java.awt.Graphics;
 import java.util.List;
+import de.hsh.Objects.*;
 
 public class GameScreen extends Screen {
 	
+	private de.hsh.Objects.Player player;
 	private static final long serialVersionUID = 1L;
 	private List<Battlefield> battlefields;
 	private float time;
 	
 	public GameScreen(List<Battlefield> pBattlefields){
-		
+		player = new de.hsh.Objects.Player();
 		battlefields = pBattlefields;
 	}
 	
 	private void update(float pDeltaTime){
 		
 		time += pDeltaTime;
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		player.draw(g);
+		
 	}
 }
