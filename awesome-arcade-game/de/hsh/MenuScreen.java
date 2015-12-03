469c6fa3c2ef523cdefb4ef26e5682ac90796b72
Gorpsel <Gorpsel@141.71.95.62>
2015-12-03 15:04:44 +0100

    (Cocken und Legionary).setHintergrund(ohrlaub);

--- awesome-arcade-game/de/hsh/MenuScreen.java
+++ awesome-arcade-game/de/hsh/MenuScreen.java
@@ -1,66 +1,99 @@
 package de.hsh;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MenuScreen extends Screen{
+public class MenuScreen extends Screen {
 	private static final long serialVersionUID = 1L;
-	
-	
-	public MenuScreen(final Main main){
+	private Image img;
+
+	public MenuScreen(final Main main) {
+
+		// ImageIcon image = new ImageIcon("/image/ohrlaub.jpg");
+		// JLabel bglabel = new JLabel();
+		// bglabel.setLayout(null);
+		// bglabel.setIcon(image);
+		// bglabel.setVisible(true);
+		// this.add(bglabel);
+		// this.setVisible(true);
+		// JFrame menu= new JFrame();
+		// menu.getContentPane().add(this);
+
+		this.img = new ImageIcon("image/ohrlaub.jpg").getImage();
+		Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+		setPreferredSize(size);
+		setMinimumSize(size);
+		setMaximumSize(size);
+		setSize(size);
+		setLayout(null);
+
 		JButton newGameBtn = new JButton("Neues Spiel");
 		JButton highscoreBtn = new JButton("Highscore");
 		JButton creditsBtn = new JButton("Credits");
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		add(Box.createRigidArea(new Dimension(0,200)));
+		add(Box.createRigidArea(new Dimension(0, 200)));
 		add(newGameBtn);
 		newGameBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-		add(Box.createRigidArea(new Dimension(0,50)));
+		add(Box.createRigidArea(new Dimension(0, 50)));
 		add(Box.createHorizontalGlue());
 		add(highscoreBtn);
 		highscoreBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-		add(Box.createRigidArea(new Dimension(0,50)));
+		add(Box.createRigidArea(new Dimension(0, 50)));
 		add(creditsBtn);
 		creditsBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-		newGameBtn.addActionListener(new ActionListener(){
+		newGameBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				GameScreen gameScreen = new GameScreen(createBattlefields());
 				main.setScreen(gameScreen);
 				setVisible(false);
 				gameScreen.setFocusable(true);
 				gameScreen.requestFocus();
-				
+
 			}
 		});
-		}
+	}
+
 	public List<Battlefield> createBattlefields() {
 		List<Battlefield> fields = new ArrayList<Battlefield>();
 		Battlefield field = new Battlefield();
-		
+
 		field.addPoint(0, 0);
-		field.addPoint(Main.SIZE,0);
+		field.addPoint(Main.SIZE, 0);
 		field.addPoint(Main.SIZE, Main.SIZE);
-		field.addPoint(Main.SIZE/2, Main.SIZE);
-		field.addPoint(Main.SIZE/2, Main.SIZE/2);
-		field.addPoint(0, Main.SIZE/2);
-				
-		
+		field.addPoint(Main.SIZE / 2, Main.SIZE);
+		field.addPoint(Main.SIZE / 2, Main.SIZE / 2);
+		field.addPoint(0, Main.SIZE / 2);
+
 		fields.add(field);
-		
+
 		return fields;
 	}
+
+	public void paintComponent(Graphics g) {
+		g.drawImage(img, 0, 0, null);
+	}
+
 }
