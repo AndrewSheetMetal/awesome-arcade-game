3da436b525460e3b6188e036785cb803d009e9a3
Alex <Alex@141.71.82.68>
2016-01-06 16:41:22 +0100

    ich kotze

--- awesome-arcade-game/de/hsh/HelpControlScreen2.java
+++ awesome-arcade-game/de/hsh/HelpControlScreen2.java
@@ -76,7 +76,7 @@ public class HelpControlScreen2 extends Screen  {
 				"Dieses Power-Up sorgt dafuer das der Spieler Zeit gutgeschrieben bekommt",
 				350, 300, g2d);
 		drawCenteredString("image/stand.png",
-				"Dieses Power-Up sogt dafuer das der Spieler sich für kurze Zeit schneller bewegt",
+				"Dieses Power-Up sogt dafuer das der Spieler sich fuer kurze Zeit schneller bewegt",
 				350, 400, g2d);
 		g2d.setFont(g2d.getFont().deriveFont(g2d.getFont().getSize() * 1.15F));
 		drawCenteredString("Seite 2 von 3", 350, 650, g2d);
