ffb0f99b2b7f18207c89121b85dc3cedc22f19fa
jeigler94 <bastiangoldbeck@googlemail.com>
2016-01-07 15:40:46 +0100

    menuScreen

--- awesome-arcade-game/de/hsh/MenuScreen.java
+++ awesome-arcade-game/de/hsh/MenuScreen.java
@@ -8,9 +8,14 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -41,133 +46,206 @@ public class MenuScreen extends Screen {
 	private Image img;
 
 	public MenuScreen(final Main main) {
-
-		// ImageIcon image = new ImageIcon("/image/ohrlaub.jpg");
-		// JLabel bglabel = new JLabel();
-		// bglabel.setLayout(null);
-		// bglabel.setIcon(image);
-		// bglabel.setVisible(true);
-		// this.add(bglabel);
-		// this.setVisible(true);
-		// JFrame menu= new JFrame();
-		// menu.getContentPane().add(this);
-
-		this.img = new ImageIcon("image/ohrlaub.jpg").getImage();
-		//Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+		
+		
+	
+		this.img = new ImageIcon("image/balls_nigga.jpg").getImage();
+		
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+	
+		
+		//Vertikale BOX b1 fuer die Buttons, in der Mitte angelegt
+		
+		//Box b1= Box.createVerticalBox();
+		
 		Dimension size = new Dimension(Main.SIZE, Main.SIZE);
 		setPreferredSize(size);
 		setMinimumSize(size);
 		setMaximumSize(size);
 		setSize(size);
-		setLayout(null);
 		
-		//CircleButton bsp = new CircleButton("Start", 250, 250, 100,100, Color.RED, Color.WHITE);
-		//add(bsp);
-		JButton newGameBtn = new JButton("Neues Spiel");
-		JButton highscoreBtn = new JButton("Highscore");
-		JButton creditsBtn = new JButton("Credits");
-		JButton hilfeBtn = new JButton("Hilfe/Steuerung");
-		JButton endeBtn = new JButton("Beenden");
+		//Runde Buttons initialisieren
 		
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		add(Box.createRigidArea(new Dimension(0, 200)));
+		CircleButton newGameBtn = new CircleButton("Start",(getWidth()/2)+40 ,50,80,80,Color.BLUE, Color.BLACK);
+		CircleButton highscoreBtn = new CircleButton("Highscore",(getWidth()/2)+40,50,80,80,Color.BLUE, Color.BLACK);
+		CircleButton creditsBtn = new CircleButton("Credits", (getWidth()/2)+40,50,80,80,Color.BLUE, Color.BLACK);
+		CircleButton hilfeBtn = new CircleButton("Hilfe/Steuerung",(getWidth()/2)+40,50,80,80,Color.BLUE, Color.BLACK);
+		CircleButton endeBtn = new CircleButton("Beenden",(getWidth()/2)+40,50,80,80,Color.BLUE, Color.BLACK);
 		
+
+		//Buttons zum Layout hinzufuegen
 		
 		add(newGameBtn);
-		newGameBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
-		add(Box.createRigidArea(new Dimension(0, 50)));
-		add(Box.createHorizontalGlue());
-		
 		add(highscoreBtn);
-		highscoreBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-		add(Box.createRigidArea(new Dimension(0, 50)));
-		
 		add(creditsBtn);
-		creditsBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
-		add(Box.createRigidArea(new Dimension(0, 50)));
-		add(Box.createHorizontalGlue());
-		
 		add(hilfeBtn);
-		hilfeBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
-		add(Box.createRigidArea(new Dimension(0, 50)));
-		add(Box.createHorizontalGlue());
-		
 		add(endeBtn);
-		endeBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
-		newGameBtn.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
 
+		newGameBtn.addMouseListener(new MouseListener() {
+		
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
 				//SVEN: GameScreen mit Level 1 initialisieren
 				GameScreen gameScreen = new GameScreen(main.createBattlefields(1), 1, main);
 				main.setScreen(gameScreen);
 				setVisible(false);
 				gameScreen.setFocusable(true);
 				gameScreen.requestFocus();
+				
+			}
 
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
 			}
 		});
 		
-		highscoreBtn.addActionListener(new ActionListener() {
+		highscoreBtn.addMouseListener(new MouseListener() {
+
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
+			public void mouseClicked(MouseEvent e) {
 				HighscoreScreen highscoreScreen = new HighscoreScreen(main);
-			
+				
 				main.setScreen(highscoreScreen);
 				setVisible(false);
 				highscoreScreen.setFocusable(true);
 				highscoreScreen.requestFocus();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
 				
 			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+
+		
+			
 			
 		});
 		
-		creditsBtn.addActionListener(new ActionListener() {
+		creditsBtn.addMouseListener(new MouseListener() {
+
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				CreditsScreen creditsScreen = new CreditsScreen(main);
+			public void mouseClicked(MouseEvent e) {
 				
+				CreditsScreen creditsScreen = new CreditsScreen(main);
 				main.setScreen(creditsScreen);
 				setVisible(false);
 				creditsScreen.setFocusable(true);
 				creditsScreen.requestFocus();
 				
+				
 			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+	
 			
 		});
 		
-		hilfeBtn.addActionListener(new ActionListener() {
+		endeBtn.addMouseListener(new MouseListener() {
+
 			@Override
-			public void actionPerformed(ActionEvent arg0){
-				HelpControlScreen hcScreen = new HelpControlScreen(main);
-				
-				main.setScreen(hcScreen);
-				setVisible(false);
-				hcScreen.setFocusable(true);
-				hcScreen.requestFocus();
-				
-			}
-			
-		});
-		
-		endeBtn.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
+			public void mouseClicked(MouseEvent arg0) {
 				beendenDialog();
+				
 			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			
+			
 		});
 		
 	}
 
 	public void beendenDialog() {
-		Object[] options = {"Ohhhh, jaa",
+		Object[] options = {"Ohhhh, jaa man!!",
                 "Nein, noch nicht"};
 		int n = JOptionPane.showOptionDialog(null,
-		    "Möchten Sie das Spiel wirklich beenden?",
+		    "Sicher?",
 		    "Beenden?",
 		    JOptionPane.YES_NO_CANCEL_OPTION,
 		    JOptionPane.QUESTION_MESSAGE,
@@ -184,7 +262,10 @@ public class MenuScreen extends Screen {
 	
 
 	public void paintComponent(Graphics g) {
+		
 		g.drawImage(img, 0, 0, null);
+		//Skalierung
+		g.translate((getWidth()-700)/2,(getHeight()-700)/2);
 	}
 
 }
